6231935b62042f2b851af949584193ab7c6c0839
NguyenRichard <36001056+NguyenRichard@users.noreply.github.com>
2019-05-31 15:16:43 +0200

    Adding imGUI-SFML binding

--- ReignTillSalvation/ReignTillSalvation/ReignTillSalvation.cpp
+++ ReignTillSalvation/ReignTillSalvation/ReignTillSalvation.cpp
@@ -4,14 +4,79 @@
 #include <iostream>
 #include "../RTSLibrary/Individual.h"
 #include "../RTSLibrary/Strong.h"
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+#define WINDOW_HEIGHT 1000
+#define WINDOW_WIDTH 1500
 
 int main()
 {
-	{
-		Individual one{ std::make_unique<Strong>("Roger") };
-		one.action();
-		one.changeState();
-		one.action();
+	/*Individual one{ std::make_unique<Strong>("Roger") };
+	one.action();
+	one.changeState();
+	one.action();*/
+
+	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "");
+	window.setVerticalSyncEnabled(true);
+	ImGui::SFML::Init(window);
+
+	sf::Color bgColor;
+
+	float color[3] = { 0.f, 0.f, 0.f };
+
+	// let's use char array as buffer, see next part
+	// for instructions on using std::string with ImGui
+	char windowTitle[255] = "ImGui + SFML = <3";
+
+	window.setTitle(windowTitle);
+	window.resetGLStates(); // call it if you only draw ImGui. Otherwise not needed.
+	sf::Clock deltaClock;
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			ImGui::SFML::ProcessEvent(event);
+
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		ImGui::SFML::Update(window, deltaClock.restart());
+
+		ImGui::Begin("Sample window"); // begin window
+		ImGui::SetWindowSize(sf::Vector2f(window.getSize().
+			x/ 4, window.getSize().y), 0);
+		ImGui::SetWindowFontScale(window.getSize().y / 1000);
+		// Background color edit
+		if (ImGui::ColorEdit3("Background color", color)) {
+			// this code gets called if color value changes, so
+			// the background color is upgraded automatically!
+			bgColor.r = static_cast<sf::Uint8>(color[0] * 255.f);
+			bgColor.g = static_cast<sf::Uint8>(color[1] * 255.f);
+			bgColor.b = static_cast<sf::Uint8>(color[2] * 255.f);
+		}
+
+		// Window title text edit
+		ImGui::InputText("Window title", windowTitle, 255);
+
+		if (ImGui::Button("Update window title")) {
+			// this code gets if user clicks on the button
+			// yes, you could have written if(ImGui::InputText(...))
+			// but I do this to show how buttons work :)
+			window.setTitle(windowTitle);
+		}
+		ImGui::End(); // end window
+
+		window.clear(bgColor); // fill background with color
+		ImGui::SFML::Render(window);
+		window.display();
 	}
+
+	ImGui::SFML::Shutdown();
 
 }
