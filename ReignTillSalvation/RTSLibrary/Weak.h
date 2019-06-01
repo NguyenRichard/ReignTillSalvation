fbe82e14aae77490f5e49d8e3839bf113a882a20
Cyprien <cyprien.lambert@outlook.fr>
2019-06-01 20:15:56 +0200

    Début d'implémentation de 'sprite' pour Element

--- ReignTillSalvation/RTSLibrary/Weak.h
+++ ReignTillSalvation/RTSLibrary/Weak.h
@@ -3,7 +3,7 @@
 
 class Weak : public IndividualState {
 public:
-	Weak(const IndividualState & state) : IndividualState(state) {};
+	Weak(const IndividualState &state) : IndividualState(state) {};
 	std::unique_ptr<IndividualState> changeState() override;
 	void action() override;
 };
