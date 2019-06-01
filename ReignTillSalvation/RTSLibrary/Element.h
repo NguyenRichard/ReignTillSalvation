88fcbc6240df133e799e12a07bae0cfc0daa8d65
Cyprien <cyprien.lambert@outlook.fr>
2019-06-01 20:15:56 +0200

    Début d'implémentation de 'sprite' pour Element

--- ReignTillSalvation/RTSLibrary/Element.h
+++ ReignTillSalvation/RTSLibrary/Element.h
@@ -1,12 +1,19 @@
 #pragma once
+
 #include <SFML\Graphics.hpp>
+
 #include "Attraction.h"
 
 class Element
 {
 public:
 	Element(std::string name, std::vector<sf::Vector2f> coords, float range) :
-		name(name), coords(coords), range(range) {};
+		name(name), coords(coords), range(range) {
+		sprite = sf::CircleShape(range);
+		sprite.setFillColor(sf::Color(255, 255, 255, 0));
+		sprite.setOutlineThickness(2);
+		sprite.setOutlineColor(sf::Color(255, 255, 255, 255));
+	};
 
 	void updateRejections();
 	void updateAttractions();
@@ -16,5 +23,6 @@ private:
 	std::vector<sf::Vector2f> coords;
 	float range;
 	std::vector<Attraction*> attractions;
+	sf::CircleShape sprite;
 };
 
