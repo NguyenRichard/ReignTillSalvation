260edb80eeeb74f907846a62ba25e5937f096647
NguyenRichard <36001056+NguyenRichard@users.noreply.github.com>
2019-06-01 17:35:16 +0200

    Now Play button works

--- ReignTillSalvation/RTSLibrary/RTSState.h
+++ ReignTillSalvation/RTSLibrary/RTSState.h
@@ -4,7 +4,7 @@
 
 class RTSState{
 public:
-	RTSState(int width, int height) : width(width), height(height) {};
+	RTSState(int width, int height) : width(width), height(height), state(false) {};
 	virtual ~RTSState() = default;
 	virtual std::unique_ptr<RTSState> changeState() = 0;
 	virtual void processInput(sf::RenderWindow& window) = 0;
@@ -12,8 +12,10 @@ public:
 	virtual void init() = 0;
 	int getHeight() const { return height; };
 	int getWidth() const { return width; };
+	bool haveToChangeState() const { return state; };
 protected:
 	int height;
 	int width;
+	bool state; //true when state need to be changed.
 
 };
