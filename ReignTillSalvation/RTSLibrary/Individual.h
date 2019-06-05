348df4fff8f0e8ed478bebd49b72b2a8259fb2b9
NguyenRichard <36001056+NguyenRichard@users.noreply.github.com>
2019-06-05 23:03:12 +0200

    Adding two units tests as well as solving some bugs

--- ReignTillSalvation/RTSLibrary/Individual.h
+++ ReignTillSalvation/RTSLibrary/Individual.h
@@ -13,7 +13,7 @@ public:
 	IndividualState* getState() const { return state.get(); };
 	void setCoord(const sf::Vector2f& new_coord) { state->setCoord(new_coord); };
 	sf::Vector2f& getCoord() const { return state->getCoord(); };
-	void changeColor(sf::Color color);
+	void changeColor(const sf::Color& color);
 	float distanceToPoint(const sf::Vector2f &point) const { return state->distanceToPoint(point); };
 	float distanceToIndividual(const Individual &individual) const;
 	float distanceToIndividual(const IndividualState& const individual) const;
