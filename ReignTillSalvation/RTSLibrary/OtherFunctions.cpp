a3973f38c837d1b40cc03c5009ea16b585698295
Cyprien <cyprien.lambert@outlook.fr>
2019-06-05 18:35:41 +0200

    ajout du sort dans Strong

--- ReignTillSalvation/RTSLibrary/OtherFunctions.cpp
+++ ReignTillSalvation/RTSLibrary/OtherFunctions.cpp
@@ -3,6 +3,6 @@
 int randomint(int const &n) {
 	static std::random_device rd;
 	static std::default_random_engine engine(rd());
-	std::uniform_int_distribution<> distribution(1, n);
+	std::uniform_int_distribution<> distribution(0, n);
 	return distribution(engine);
 }
