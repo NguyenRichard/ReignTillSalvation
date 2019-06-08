8c031fd6973226eb35896049811b0dd608589ef0
Cyprien <cyprien.lambert@outlook.fr>
2019-06-08 23:24:38 +0200

    réparation du temps dans le changement de game state

--- ReignTillSalvation/RTSLibrary/Game.cpp
+++ ReignTillSalvation/RTSLibrary/Game.cpp
@@ -20,10 +20,10 @@ Game::Game(const RTSState& state) :
 	time(std::make_unique<sftools::Chronometer>(sf::Time::Zero))
 {}
 
-Game::Game(const Game & state, std::unique_ptr<Map> new_map, std::unique_ptr<sftools::Chronometer> time) :
+Game::Game(const Game & state, std::unique_ptr<Map> new_map, std::unique_ptr<sftools::Chronometer> new_time) :
 	RTSState(state), 
 	map(std::move(new_map)),
-	time(std::move(time))
+	time(std::move(new_time))
 {}
 
 #pragma endregion RTSStateFunctions
