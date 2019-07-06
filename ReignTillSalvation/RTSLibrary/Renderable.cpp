133dc35c439d46b94458c72a627b870ca1fc49ab
NguyenRichard <36001056+NguyenRichard@users.noreply.github.com>
2019-07-06 11:38:21 +0200

    creating renderable methods

---
+++ ReignTillSalvation/RTSLibrary/Renderable.cpp
@@ -0,0 +1,13 @@
+#include "Renderable.h"
+
+void Renderable::render(sf::RenderWindow& window) {
+	for (const auto& drawable : drawables) {
+		window.draw(*drawable.first);
+	}
+}
+
+void Renderable::updateAnimation() {
+	if (animcount == -1) {
+		return;
+	}
+}
--- ReignTillSalvation/RTSLibrary/Renderable.h
+++ ReignTillSalvation/RTSLibrary/Renderable.h
@@ -7,9 +7,9 @@ class Renderable {
 private:
 	std::vector <std::pair<std::unique_ptr<sf::Drawable>, std::pair<std::vector<sf::Texture*>,int>>> drawables;
 	ObjectLogic* object;
-	int animcount; // if animcount = -1, no animation.
+	int animcount; // if animcount = -1, no animation. (WARNING: Drawable must be a sprite if animcount != -1)
 public:
-	void render();
+	void render(sf::RenderWindow& window);
 	void updateDrawables();
 	void updateAnimation();
 };
